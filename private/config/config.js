f77f086a72fe0b891ff7a52e81cac9062a342461
MrFil27 <78660195+MrFil27@users.noreply.github.com>
2025-09-04 12:21:46 +0200

    Deleted file

--- private/config/config.js
+++
@@ -1,9 +0,0 @@
-const CONFIG = {
-    calendar: {
-        locale: "it", //"en", "fr", "es" etc.
-
-        schoolStart: "15-09-2024",
-        schoolEnd: "30-07-2025", //10-06-2025
-        dateFormat: "d-m-Y",
-    }
-};
